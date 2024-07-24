5345759ff147cd7bf9f2054bfc740d45dc9641c6
nqc3802 <84844943+nqc3802@users.noreply.github.com>
2024-07-24 11:52:26 +0700

    delete

--- leave/LeaveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.timesheet.leave;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeaveRepository extends JpaRepository<Leave, Integer> {
-
-}
