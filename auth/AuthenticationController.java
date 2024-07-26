41e5518b6a747095e6f3454309ecc872c8ff83d7
nqc3802 <nqc3822@gmail.com>
2024-07-26 11:27:58 +0700

    email service

--- auth/AuthenticationController.java
+++ auth/AuthenticationController.java
@@ -2,11 +2,13 @@ package com.example.timesheet.auth;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.timesheet.email.EmailService;
 import com.example.timesheet.user.User;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
+    @Autowired
+    EmailService emailService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody User request) {
+        emailService.sendEmail(request.getEmail(), "Welcome to Timesheet", "You have successfully registered to Timesheet");
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
