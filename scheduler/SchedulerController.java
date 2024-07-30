3c8bfd75691948e23d09dcba9256e6a2ab414054
nqc3802 <nqc3822@gmail.com>
2024-07-30 11:35:13 +0700

    dynamic cronjob

--- scheduler/SchedulerController.java
+++ scheduler/SchedulerController.java
@@ -1,4 +1,4 @@
-package com.example.timesheet.config;
+package com.example.timesheet.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.timesheet.config.DynamicSchedulerConfig;
 
 @RestController
 @RequestMapping("/api/v1/scheduler")
