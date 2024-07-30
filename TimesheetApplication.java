af516107256facc08d2b4aa7038d07253b4e0a8b
nqc3802 <nqc3822@gmail.com>
2024-07-30 22:40:14 +0700

    update send scheduled email

--- TimesheetApplication.java
+++ TimesheetApplication.java
@@ -2,8 +2,10 @@ package com.example.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TimesheetApplication {
 
 	public static void main(String[] args) {
--- email/EmailService.java
+++ email/EmailService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Async
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("nqc3822@gmail.com");
