f52e9540ce3f54579ac70e79c27a1baa07acb8dc
nqc3802 <nqc3822@gmail.com>
2024-07-30 11:32:49 +0700

    dynamic cronjob

---
+++ config/SchedulerController.java
@@ -0,0 +1,24 @@
+package com.example.timesheet.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/scheduler")
+public class SchedulerController {
+
+    @Autowired
+    private DynamicSchedulerConfig dynamicSchedulerConfig;
+
+    @PreAuthorize("hasRole('ROLE_Admin')")
+    @PostMapping("/updateCron")
+    public ResponseEntity<String> updateCronExpression(@RequestParam String newCronExpression) {
+        dynamicSchedulerConfig.updateCronSchedule(newCronExpression);
+        return ResponseEntity.ok("Cron expression updated successfully");
+    }
+}
